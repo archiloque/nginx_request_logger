63ca72d9d74dd403e77172cc2bad760aed08384c
julienkirch <julienkirch@MacBook-Pro-de-OCTO-FORMATION.local>
2015-06-30 23:19:58 +0200

    Move rest code to separate file

--- nginx_services_logging.lua
+++ nginx_services_logging.lua
@@ -5,70 +5,7 @@ cjson = require("cjson")
 uuid = require("uuid")
 uuid.randomseed(ngx.now())
 
--- Rest services
-local RestServiceLogging = {}
-RestServiceLogging.__index = RestServiceLogging
-
-function RestServiceLogging.new(configuration)
-    local self = setmetatable({}, RestServiceLogging)
-    self.name = configuration.name
-    self.uri = configuration.uri
-    self.uri_type = (configuration.uri_type or 'GET')
-    self.method = configuration.method
-    if configuration.request and (next(configuration.request) ~= nill) then
-        self.request = configuration.request
-    end
-    if configuration.response and (next(configuration.response) ~= nill) then
-        self.response = configuration.response
-    end
-
-    self.description = "REST [" .. self.name .. "] "
-    return self
-end
-
--- Check it the current call match the service
-function RestServiceLogging.match_current_call(self)
-    if ngx.req.get_method() == self.method then
-        if ngx.var.uri == self.uri then
-            return true
-        end
-    else
-        return false
-    end
-end
-
-function RestServiceLogging.process_before_call(self)
-    return self:process_call(ngx.req.get_headers(), ngx.var.request_body, self.request)
-end
-
-function RestServiceLogging.process_after_call(self)
-    return self:process_call(ngx.resp.get_headers(), table.concat(ngx.ctx.buffered_content), self.response)
-end
-
-function RestServiceLogging.process_call(self, headers, raw_body, parameters)
-    if parameters then
-        local content_type = headers["Content-Type"]
-        if (content_type == "application/json") then
-            if raw_body then
-                local parsed_body = cjson.decode(raw_body)
-                local result_params = {}
-                for index, param_configuration in ipairs(parameters) do
-                    result_params[param_configuration.name] = parsed_body[param_configuration.path]
-                end
-                return result_params
-            end
-        end
-    end
-    return nill
-end
-
-function RestServiceLogging.description(self)
-    return self.description
-end
-
-function RestServiceLogging.require_response(self)
-    return self.response
-end
+local RestServiceLogging = require("nginx_services_logging_rest")
 
 local NginxServicesLogging = {}
 NginxServicesLogging.__index = NginxServicesLogging
